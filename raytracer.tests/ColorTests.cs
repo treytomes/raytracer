341a3b3fdd0f56e57fcbf582bdcdaedc68593d4f
Trey Tomes <trey.tomes@gmail.com>
2020-02-20 09:53:56 -0600

    Add a Color struct with unit tests.

---
+++ raytracer.tests/ColorTests.cs
@@ -0,0 +1,51 @@
+﻿using raytracer.math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace raytracer.tests
+{
+	public class ColorTests
+	{
+		[Fact(DisplayName = "Colors are tuples.")]
+		public void Can_construct_a_color()
+		{
+			var c = new Color(-0.5f, 0.4f, 1.7f);
+			Assert.Equal(-0.5f, c.Red);
+			Assert.Equal(0.4f, c.Green);
+			Assert.Equal(1.7f, c.Blue);
+		}
+
+		[Fact(DisplayName = "Adding colors.")]
+		public void Can_add_colors()
+		{
+			var c1 = new Color(0.9f, 0.6f, 0.75f);
+			var c2 = new Color(0.7f, 0.1f, 0.25f);
+			Assert.Equal(new Color(1.6f, 0.7f, 1.0f), c1 + c2);
+		}
+
+		[Fact(DisplayName = "Subtracting colors.")]
+		public void Can_subtract_colors()
+		{
+			var c1 = new Color(0.9f, 0.6f, 0.75f);
+			var c2 = new Color(0.7f, 0.1f, 0.25f);
+			Assert.Equal(new Color(0.2f, 0.5f, 0.5f), c1 - c2);
+		}
+
+		[Fact(DisplayName = "Multiplying a color by a scalar.")]
+		public void Can_scale_colors()
+		{
+			var c = new Color(0.2f, 0.3f, 0.4f);
+			Assert.Equal(new Color(0.4f, 0.6f, 0.8f), c * 2);
+		}
+
+		[Fact(DisplayName = "Multiplying colors.")]
+		public void Can_multiply_colors()
+		{
+			var c1 = new Color(1.0f, 0.2f, 0.4f);
+			var c2 = new Color(0.9f, 1.0f, 0.1f);
+			Assert.Equal(new Color(0.9f, 0.2f, 0.04f), c1 * c2);
+		}
+	}
+}
---
+++ raytracer/math/Color.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace raytracer.math
+{
+	/// <remarks>
+	/// While it only makes sense to render colors with components in [0, 1], don't constrain the components to that range.
+	/// The math being done on these colors will require that the values go out of range from time to time.
+	/// </remarks>
+	public struct Color : IEquatable<Color>
+	{
+		#region Fields
+
+		public readonly float Red;
+		public readonly float Green;
+		public readonly float Blue;
+
+		#endregion
+
+		#region Constructors
+
+		public Color(float red, float green, float blue)
+		{
+			Red = red;
+			Green = green;
+			Blue = blue;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public override string ToString()
+		{
+			return $"Color({Red}, {Green}, {Blue})";
+		}
+
+		public bool Equals(Color other)
+		{
+			return MathHelper.Equals(Red, other.Red) && MathHelper.Equals(Green, other.Green) && MathHelper.Equals(Blue, other.Blue);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Color))
+			{
+				return false;
+			}
+			return Equals((Color)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		#endregion
+
+		#region Operators
+
+		public static bool operator ==(Color left, Color right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(Color left, Color right)
+		{
+			return !left.Equals(right);
+		}
+
+		public static Color operator +(Color left, Color right)
+		{
+			return new Color(left.Red + right.Red, left.Green + right.Green, left.Blue + right.Blue);
+		}
+
+		public static Color operator -(Color left, Color right)
+		{
+			return new Color(left.Red - right.Red, left.Green - right.Green, left.Blue - right.Blue);
+		}
+
+		public static Color operator *(Color t, float scale)
+		{
+			return new Color(t.Red * scale, t.Green * scale, t.Blue * scale);
+		}
+
+		public static Color operator *(float scale, Color c)
+		{
+			return new Color(c.Red * scale, c.Green * scale, c.Blue * scale);
+		}
+
+		public static Color operator /(Color c, float scale)
+		{
+			return new Color(c.Red / scale, c.Green / scale, c.Blue / scale);
+		}
+
+		/// <summary>
+		/// Calculate the Hadamard / Schur product of 2 colors.
+		/// </summary>
+		public static Color operator *(Color left, Color right)
+		{
+			return new Color(left.Red * right.Red, left.Green * right.Green, left.Blue * right.Blue);
+		}
+
+		#endregion
+	}
+}
